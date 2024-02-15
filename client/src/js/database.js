dc7f53c76725e89ffaeb6ab7c2fe9e6c2085fd24
astro <angelica.strong0725@gmail.com>
2024-02-14 23:31:04 -0800

    reroll back to previous commit

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,20 +13,28 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) =>
-{
-  const db = await openDB(DB_NAME, 1);
-  const keyOfUpdatedIndex = await db.put(OBJ_STORE_NAME, {id: 1, value: content});
-  console.log(`Saved to database index ${keyOfUpdatedIndex}:\n"""\n${await db.get(OBJ_STORE_NAME, keyOfUpdatedIndex)}\n"""`);
+export const putDb = async (content) => {
+  console.log('PUT to the database');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({ id: 1, value: content });
+  const result = await request;
+  console.log('Data saved to the database', result.value);
 };
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () =>
-{
-  const db = await openDB(DB_NAME, 1);
-  const content = await db.get(OBJ_STORE_NAME, 1);
-  console.log(content !== undefined ? `Retrieved from database:\n"""\n${content}\n"""` : "Data not found in the database");
-  return content;
+export const getDb = async () => {
+  console.log('GET from the database');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.get(1);
+  const result = await request;
+  result
+    ? console.log('Data retrieved from the database', result.value)
+    : console.log('Data not found in the database');
+  return result?.value;
 };
 
 initdb();
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -8,58 +8,61 @@ const { InjectManifest } = require('workbox-webpack-plugin');
 
 module.exports = () => {
   return {
-    mode: "development",
+    mode: 'development',
     entry: {
-      main: "./src/js/index.js",
-      install: "./src/js/install.js"
+      main: './src/js/index.js',
+      install: './src/js/install.js'
     },
     output: {
-      filename: "[name].bundle.js",
-      path: path.resolve(__dirname, "dist"),
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
       new HtmlWebpackPlugin({
-        template: "./index.html"
+        template: './index.html',
+        title: 'J.A.T.E'
       }),
       new InjectManifest({
-        swSrc: "./src-sw.js",
-        swDest: "src-sw.js"
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
       }),
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
-        name: "Pwatext",
-        description: "Progressive web app text editor",
-        background_color: "#fff",
-        theme_color: "#fff",
-        start_url: "/",
-        publicPath: "/",
+        name: 'Just Another Text Editor',
+        short_name: 'J.A.T.E',
+        description: 'Takes notes with JavaScript syntax highlighting!',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: '/',
+        publicPath: '/',
         icons: [
           {
-            src: path.resolve("src/images/logo.png"),
+            src: path.resolve('src/images/logo.png'),
             sizes: [96, 128, 192, 256, 384, 512],
-            destination: path.join("assets", "icons"),
+            destination: path.join('assets', 'icons'),
           },
         ],
-      })
+      }),
     ],
 
     module: {
       rules: [
         {
           test: /\.css$/i,
-          use: ["style-loader", "css-loader"],
+          use: ['style-loader', 'css-loader'],
         },
         {
-          test: /\.js$/,
+          test: /\.m?js$/,
           exclude: /node_modules/,
           use: {
-            loader: "babel-loader",
+            loader: 'babel-loader',
             options: {
-              presets: ["@babel/preset-env"]
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
             },
-          }
-        }
+          },
+        },
       ],
     },
   };
